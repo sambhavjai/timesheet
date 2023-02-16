27ce570ae80d5f4f626369daacd9c0f672355827
Siraj Alam <sirajalam049@gmail.com>
2023-02-16 21:09:18 +0530

    Initial commit

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAcmWuZmhTddUIjYspQcndhkfeeKdUUYlU",
+  authDomain: "timesheet-f5623.firebaseapp.com",
+  projectId: "timesheet-f5623",
+  storageBucket: "timesheet-f5623.appspot.com",
+  messagingSenderId: "914307607321",
+  appId: "1:914307607321:web:ddff60060c218ba8f37388"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
